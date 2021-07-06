f079bd2a73edffee7846e52b4c5268a6d64466a1
TANNER MECK <steve.meck246@gmail.com>
2021-07-06 14:19:48 -0700

    html added and arrays created

---
+++ dirtbike.js
@@ -0,0 +1,58 @@
+//create an array
+const ktm {
+    id: 'ktm',
+    name: 'ktm',
+    image: 'ktm.jpeg',
+    description:'2021 ktm 450sxf',
+    category: 'first-place',
+    price: 105000
+
+};
+
+const yamaha {
+    id: 'yamaha',
+    name: 'yamaha',
+    image: 'yamaha.jpeg',
+    description: '2021 yamaha 450f',
+    category: 'second-place',
+    price: 9999
+
+};
+
+const honda {
+    id: 'honda',
+    name: 'honda',
+    image: 'honda.jpeg',
+    description: '2021 honda crf450r',
+    category: 'third-place',
+    price: 9999
+
+};
+
+const suzuki {
+    id: 'suzuki',
+    name: 'suzuki',
+    image: 'suzuki.jpeg',
+    description: '2021 susuki 450f',
+    category: 'fourth-place',
+    price: 9600
+
+};
+
+const kawasaki {
+    id: 'kawasaki',
+    name: 'kawasaki',
+    image: 'kawasaki.jpeg',
+    description: '2021 kx 450f',
+    category: 'fifth-place',
+    price: 9600
+
+};
+const dirtbikes = [
+    ktm,
+    yamaha,
+    honda,
+    suzuki,
+    kawasaki
+]
+export default dirtbikes
